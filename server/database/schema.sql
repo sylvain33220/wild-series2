9937d71ac0de710926ccb4ec53c32254ad4aec82
sylvain33220 <sylvain.poteaux@gmail.com>
2024-08-05 11:36:24 +0200

    add server

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,18 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+create table category (
+  id int auto_increment primary key not null,
+  name varchar(100) 
+);
+
+create table program (
+  id int auto_increment primary key not null, 
+  title varchar(255),
+  synopsis text,
+  poster varchar(255),
+  country varchar(100),
+  year int,
+  category_id int
+
+);
